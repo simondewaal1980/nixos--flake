8a54d41915029c4669c52f245a090101fee8d80a
simondewaal1980 <119229538+simondewaal1980@users.noreply.github.com>
2024-01-02 23:18:53 +0100

    test

--- configuration.nix
+++ configuration.nix
@@ -292,51 +292,5 @@ environment.etc."vimrc".text = ''
  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
  system.stateVersion = "23.11"; # Did you read the comment?
 
-#Home manager
-
-home-manager.users.simon = { pkgs, ... }: {
- home.stateVersion = "23.11";
-nixpkgs.config.allowUnfree = true;
- home.packages = [ 
- pkgs.helix
-pkgs.rnix-lsp
- pkgs.nixpkgs-fmt
- pkgs.tree
-pkgs.nodePackages_latest.neovim
- pkgs.tree-sitter
-
-
-   ];
-  
-programs.vscode = {
-  enable = true;
-  extensions = with pkgs.vscode-extensions; [
-    dracula-theme.theme-dracula
-    vscodevim.vim
-jnoortheen.nix-ide
-bbenoist.nix
-    yzhang.markdown-all-in-one
-  ];
-};
-};
-programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    configure = {
-      customRC = ''
-        set undofile
-        set undodir=~/.vim/undodir
-      '';
-      packages.nix.start = with pkgs.vimPlugins; [  nvim-treesitter.withAllGrammars ];
-    };
-
-  };
-
-
-
-
-
 }
 
--- flake.nix
+++ flake.nix
@@ -1,25 +1,35 @@
-
-
-
 {
+  description = "NixOS configuration";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";  
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      nixos = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+      # TODO please change the hostname to your own
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x87_64-linux";
         modules = [
           ./configuration.nix
-          inputs.home-manager.nixosModules.home-manager
+
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # TODO replace ryan with your own username
+            home-manager.users.simon = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
         ];
       };
     };
   };
 }
+
