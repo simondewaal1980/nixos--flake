ad444d0359daf27b0be34ddda0eff26e0a4c3d6d
simondewaal1980 <119229538+simondewaal1980@users.noreply.github.com>
2024-01-21 16:52:10 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.url = "github:hyprwm/Hyprland";
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
